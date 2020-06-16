3b67181caa47bebd3e7cb89e527dba7e5d6effdd
Brennen Tatum <bmar566@gmail.com>
2020-06-16 16:51:11 -0500

    Initial commit

---
+++ DogGo/DogGo/Controllers/WalkerController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGo.Models;
+using DogGo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DogGo.Controllers
+{
+    
+    public class WalkerController : Controller
+    {
+        private readonly WalkerRepository _walkerRepo;
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        public WalkerController(IConfiguration config)
+        {
+            _walkerRepo = new WalkerRepository(config);
+        }
+        // GET: WalkerController
+        public ActionResult Index()
+        {
+
+            List<Walker> walkers = _walkerRepo.GetAllWalkers();
+
+            return View(walkers);
+        }
+
+        // GET: WalkerController/Details/5
+       public ActionResult Details(int id)
+        {
+            Walker walker = _walkerRepo.GetWalkerById(id);
+
+            if (walker == null)
+            {
+                return NotFound();
+            }
+
+            return View(walker);
+        }
+
+        // GET: WalkerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WalkerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WalkerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WalkerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WalkerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WalkerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
