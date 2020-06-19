74829e995b8e3ac7df41cdd2b277cbc8f722071f
Brennen Tatum <bmar566@gmail.com>
2020-06-19 09:53:20 -0500

    removed extra doggo folder

---
+++ DogGo/Reopsitories/WalkRepository.cs
@@ -0,0 +1,78 @@
+﻿using DogGo.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Reopsitories
+{
+    public class WalkRepository
+    {
+
+        private readonly IConfiguration _config;
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        public WalkRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        public List<Walk> GetWalkByWalkerId(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT w.Id, w.Date, w.Duration, w.DogId, WalkerId
+                        FROM Walks w
+                        JOIN Walker wa On wa.id = w.WalkerId
+
+                        WHERE wa.Id = @id
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Walk> walks = new List<Walk>();
+                    if (reader.Read())
+
+                    {
+                        Walk walk = new Walk
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Date = reader.GetDateTime(reader.GetOrdinal("Date")),
+                            Duration = reader.GetInt32(reader.GetOrdinal("Duration")),
+                            WalkerId = reader.GetInt32(reader.GetOrdinal("WalkerId")),
+                             DogId = reader.GetInt32(reader.GetOrdinal("DogId"))
+                        };
+
+                        
+                        walks.Add(walk);
+                      
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return null;
+                    }
+                    reader.Close();
+                    return walks;
+                }
+
+            }
+        }
+
+    }
+}
